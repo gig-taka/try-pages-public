922aa43245b9b0379e5030bb8a201d761006607e
gig-taka <masateru.takahashi@showcase-gig.com>
2024-08-06 15:45:54 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/try-pages-public",
+  assetPrefix: "/try-pages-public/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
---
+++ out/_next/static/7NQ5tLt-L-3TvC_9wiwbe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f825777e69fe7460.css","static/chunks/pages/index-4e06f3107ce7d0dd.js"],"/_error":["static/chunks/pages/_error-ecb326a48e9c6357.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
