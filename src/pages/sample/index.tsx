255f5d9fc23435e593ed1591bd0d1dda5483b353
gig-taka <masateru.takahashi@showcase-gig.com>
2024-08-06 19:43:14 +0900

    updata

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -2,17 +2,24 @@ import Link from "next/link";
 
 export const Sample = () => {
   return (
-    <div className="flex flex-col justify-center bg-gray-300 h-[100dvh] items-center">
+    <div className="flex flex-col justify-center bg-gray-800 h-[100dvh] items-center">
       <Link href="/">
         <h1 className="sm:text-4xl md:text-6xl lg:text-9xl">サンプルページ1</h1>
       </Link>
       <ul className="my-6 md:text-xl lg:text-2xl">
-        <li>{process.env.BUILD_DATETIME}</li>
+        <li aria-label="デプロイされた時間です">
+          {process.env.BUILD_DATETIME}
+        </li>
         <li className="hidden">display: none</li>
         <li className="invisible">visibility: hidden</li>
         <li>読み上げる</li>
         <li aria-hidden="true">読み上げない</li>
       </ul>
+      <div aria-label="たらこパスタです。たらこの風味を生かしたシンプルで美味しい料理です。千二百円です">
+        <div>たらこパスタ</div>
+        <div>1,200円</div>
+        <div>たらこの風味を生かしたシンプルで美味しい料理です</div>
+      </div>
       <Link href="/">
         <h1 className="text-2xl">戻る</h1>
       </Link>
