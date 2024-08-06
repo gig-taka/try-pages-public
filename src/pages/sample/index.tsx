ff24b4182a2c37a137c41fc2c67078269856e2c4
gig-taka <masateru.takahashi@showcase-gig.com>
2024-08-06 18:54:10 +0900

    aria-hidden example

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -2,9 +2,18 @@ import Link from "next/link";
 
 export const Sample = () => {
   return (
-    <div className="flex justify-center bg-gray-300 h-[100dvh] items-center">
+    <div className="flex flex-col justify-center bg-gray-300 h-[100dvh] items-center">
       <Link href="/">
         <h1 className="sm:text-4xl md:text-6xl lg:text-9xl">サンプルページ1</h1>
+      </Link>
+      <ul className="my-6">
+        <li className="hidden">display: none</li>
+        <li className="invisible">visibility: hidden</li>
+        <li>読み上げる</li>
+        <li aria-hidden="true">読み上げない</li>
+      </ul>
+      <Link href="/">
+        <h1 className="text-2xl">戻る</h1>
       </Link>
     </div>
   );
