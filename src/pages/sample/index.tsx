c764719a711543e225edd3fbecc574e421f11fe1
gig-taka <masateru.takahashi@showcase-gig.com>
2024-08-07 19:26:25 +0900

    update

--- src/pages/sample/index.tsx
+++ src/pages/sample/index.tsx
@@ -30,37 +30,6 @@ export const Sample = () => {
           <li>読み上げる</li>
           <li aria-hidden="true">読み上げない</li>
         </ul>
-        <h2>まとめて読み上げる</h2>
-        <Link href="/">
-          <div>たらこパスタ</div>
-          <div>1,200円</div>
-          <div>たらこの風味を生かしたシンプルで美味しい料理です</div>
-        </Link>
-        <h2>個別に読み上げる</h2>
-        <article>
-          <div>たらこパスタ</div>
-          <div>1,200円</div>
-          <div>たらこの風味を生かしたシンプルで美味しい料理です</div>
-        </article>
-        <h2>画像のaltに商品名を入れる</h2>
-        <img src="img.jpg" className="w-40" alt="親子丼" />
-        <h2>画像のaltに商品説明を入れる</h2>
-        <img
-          src="img.jpg"
-          className="w-40"
-          alt="親子丼は、柔らかく煮込んだ鶏肉と半熟の卵を甘辛いだしで味付けし、ふっくらとしたご飯の上にのせた日本の伝統的な丼物です。ねぎをアクセントに加え、シンプルながらも深い味わいが楽しめます。栄養バランスも良く、手軽に食べられるため、忙しい日の昼食や夕食にぴったりです。"
-        />
-        <h2>画像のaltに商品名まとめて読み上げる</h2>
-        <Link href="/" className="flex gap-2 h-[300px] justify-center">
-          <div className="flex flex-col">
-            <img src="img.jpg" alt="親子丼" className="w-[100px]" />
-            <div>親子丼</div>
-            <div>850円</div>
-          </div>
-          <div className="w-[60%]">
-            親子丼は、柔らかく煮込んだ鶏肉と半熟の卵を甘辛いだしで味付けし、ふっくらとしたご飯の上にのせた日本の伝統的な丼物です。ねぎをアクセントに加え、シンプルながらも深い味わいが楽しめます。栄養バランスも良く、手軽に食べられるため、忙しい日の昼食や夕食にぴったりです。
-          </div>
-        </Link>
         <a href="https://www.yahoo.co.jp">リンク</a>
         {!a && (
           <button
@@ -77,6 +46,37 @@ export const Sample = () => {
       {a && (
         <div className="fixed left-0 top-0 w-[100dvw] h-[100dvh] bg-blue-200 flex flex-col justify-center items-center md:text-xl lg:text-2xl">
           <h1>モーダルダイアログ</h1>
+          <h2>まとめて読み上げる</h2>
+          <Link href="/">
+            <div>たらこパスタ</div>
+            <div>1,200円</div>
+            <div>たらこの風味を生かしたシンプルで美味しい料理です</div>
+          </Link>
+          <h2>個別に読み上げる</h2>
+          <article>
+            <div>たらこパスタ</div>
+            <div>1,200円</div>
+            <div>たらこの風味を生かしたシンプルで美味しい料理です</div>
+          </article>
+          <h2>画像のaltに商品名を入れる</h2>
+          <img src="img.jpg" className="w-40" alt="親子丼" />
+          <h2>画像のaltに商品説明を入れる</h2>
+          <img
+            src="img.jpg"
+            className="w-40"
+            alt="親子丼は、柔らかく煮込んだ鶏肉と半熟の卵を甘辛いだしで味付けし、ふっくらとしたご飯の上にのせた日本の伝統的な丼物です。ねぎをアクセントに加え、シンプルながらも深い味わいが楽しめます。栄養バランスも良く、手軽に食べられるため、忙しい日の昼食や夕食にぴったりです。"
+          />
+          <h2>画像のaltに商品名まとめて読み上げる</h2>
+          <Link href="/" className="flex gap-2 h-[300px] justify-center">
+            <div className="flex flex-col">
+              <img src="img.jpg" alt="親子丼" className="w-[100px]" />
+              <div>親子丼</div>
+              <div>850円</div>
+            </div>
+            <div className="w-[60%]">
+              親子丼は、柔らかく煮込んだ鶏肉と半熟の卵を甘辛いだしで味付けし、ふっくらとしたご飯の上にのせた日本の伝統的な丼物です。ねぎをアクセントに加え、シンプルながらも深い味わいが楽しめます。栄養バランスも良く、手軽に食べられるため、忙しい日の昼食や夕食にぴったりです。
+            </div>
+          </Link>
           <button
             className="p-2 border rounded-md m-4 bg-blue-300"
             onClick={() => b(false)}
